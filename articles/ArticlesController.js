9f0da66a042675be16fbc09290ab02e9bc4fc9e7
Maria <mary.m0rhw@gmail.com>
2024-08-14 22:42:48 -0300

    Article

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Category = require('../categories/Category');
+const Article = require('./Article');
+const slugify = require('slugify');
 
 router.get('/articles', (req, res) => {
     res.send('Articles');
@@ -10,6 +12,27 @@ router.get('/admin/articles/new', (req, res) => {
     Category.findAll().then(categories => {
         res.render('admin/articles/new', {categories: categories});
     })
+});
+
+router.get('/admin/articles', (req, res) => {
+    Article.findAll().then(categories => {
+        res.render('admin/articles/index', {categories: categories});
+    })
+});
+
+router.post('/articles/save', (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;   
+    var category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: title,
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect('/articles');
+    }); 
 });
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
 const conection = new Sequelize('guiapress', 'root', '', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timeZone: '-03:00'  
 });
 
 module.exports = conection;
